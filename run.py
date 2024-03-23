af4eedb5ef2b7ae63a87c6f54ae86f7b50a5ecf5
Ayisha Sandiford <ish.equal@gmail.com>
2024-03-23 13:56:26 +0000

    Stop user from making wrong letter tries pass 6 attemps

--- run.py
+++ run.py
@@ -1,89 +1,9 @@
 import random
 
+from word_list import hidden_word_list_level_one
+
 print('Welcome to hangman')
   
-hidden_word_list_level_one = [
-'the',
-'about',
-'great',
-'learn',
-'of',
-'many',
-'where',
-'should',
-'some',
-'through',
-'world',
-'would',
-'right',
-'high',
-'look',
-'too',
-'every',
-'you',
-'two',
-'any',
-'near',
-'was',
-'more',
-'want',
-'school',
-'could',
-'also',
-'father',
-'people',
-'around',
-'earth',
-'they',
-'put',
-'eye',
-'water',
-'does',
-'light',
-'have',
-'been',
-'another',
-'thought',
-'from',
-'who',
-'large',
-'head',
-'one',
-'find',
-'even',
-'saw',
-'because',
-'what',
-'come',
-'here',
-'along',
-'all',
-'only',
-'move',
-'might',
-'were',
-'work',
-'kind',
-'something',
-'when',
-'know',
-'again',
-'always',
-'your',
-'year',
-'change',
-'both',
-'said',
-'give',
-'off',
-'often',
-'there',
-'most',
-'night',
-'very',
-'away'
-]
-
 
 random_word = random.choice(hidden_word_list_level_one).upper()
 
@@ -98,10 +18,11 @@ print(' '.join(guessed_word))
 
 user_letter = ""
 pre_guessed_letters = ""
+bad_guesses = 0
 
 
 # loop until the user guesses the word but allow the loop to break with a . 
-while '_' in guessed_word and user_letter != '.' and :
+while '_' in guessed_word and user_letter != '.' and bad_guesses < 6:
   user_letter = input('Enter a letter: ').upper()
   if user_letter in pre_guessed_letters:
     print('You have already guessed this letter')
@@ -117,5 +38,9 @@ while '_' in guessed_word and user_letter != '.' and :
       guessed_word[letterLocation] = user_letter
     print(' '.join(guessed_word))
   else:
-    print(' '.join(guessed_word))
-    print(f"You guessed the letter {user_letter} incorrectly!")
+    bad_guesses += 1
+    if bad_guesses == 0:
+      print('you lost')
+    else:
+      print(' '.join(guessed_word))
+      print(f"You guessed the letter {user_letter} incorrectly!")
