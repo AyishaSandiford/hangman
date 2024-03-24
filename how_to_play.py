20ef556c08687480de21d26aefee0322f337bbb3
Ayisha Sandiford <ish.equal@gmail.com>
2024-03-24 01:57:24 +0000

    Test text fit on terminal

--- how_to_play.py
+++ how_to_play.py
@@ -2,24 +2,17 @@ how_to_play_game = ("""
 
 How To Play
 
-The computer will generate a word randomly from a predefined list.
-
 The computer will display the word you need to guess as a series of dashes,
 with each dash representing a letter in the word. For example, if the word is 'apple,' you might see '_ _ _ _ _'.
 
-Guess Letters: Take turns guessing letters that you think might be in the word. 
-You can usually type your guesses into a text box provided by the game. 
-Start with common letters like vowels (a, e, i, o, u) or frequently used consonants (like s, t, n, r).
+Guess what you think might be in the word. Type your guesses on the dashes provided.
 
 Feedback from the Computer: After you guess a letter, the computer will tell you if the letter is in the word or not by filling in the blanks. 
 If the letter is in the word, it will reveal its position(s) in the word. 
 If not, it will display a message indicating that the letter is not in the word and will draw part of the Hangman.
+This will continue until the Hangman has being fully drawn. At this point you lose
 
-Keep Guessing: Continue guessing letters one by one until you either guess the word correctly or make too many incorrect guesses, 
-resulting in the Hangman being fully drawn.
-
-Winning and Losing: If you guess the word correctly before the Hangman is fully drawn, you win! 
-If the Hangman is fully drawn before you guess the word, you lose.
+However, if you guess the word correctly before the Hangman is fully drawn, you win! 
 
 Play Again: You will have to run the program again to play again """)
 
